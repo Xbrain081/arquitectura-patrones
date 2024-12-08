93ffc3bbe4fab8c968facc973601e9d3368e9c74
D714NRO2O <dsrozot10@gmail.com>
2024-12-08 04:37:15 -0500

    se agrego el modelo de usuarios al archivo users.js de la carpeta entities

---
+++ back/codeCore/entities/users.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(id, name, email, age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+}
+
+module.exports = User;
+
+//Porqué tan sencillo? 
+//Porque debe ser una lógica pura, lo q quiere decir q no debe depender de una logica externa
