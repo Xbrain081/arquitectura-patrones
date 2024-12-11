38a22106a54dd7ac3c6ba6129b893e4a47a43cad
D714NRO2O <dsrozot10@gmail.com>
2024-12-11 18:20:19 -0500

    se agrega el archivo .env

--- back/server.js
+++ back/server.js
@@ -1,13 +1,3 @@
-// clean arquitecture: 
-// 1 organizacion de carpetas
-// 2 implementar sequelize en el codigo
-// 3 mejorar las respuestas del los metodos
-// 4 crear frontend 
-// rule: - de la rama principal (master) sacar otra rama si se hace una modificacion
-//       - se sube la rama creada a git 
-//       - se realiza el approbal de los participantes 
-//       - se mergea la rama que saco uno de los participantes 
-
 const express = require('express');
 const Database = require('./codeCore/infraestructure/db');
 const UserRepository = require('./codeCore/interface/repositories/userRepository');
@@ -23,11 +13,9 @@ app.use(express.json());
 
 const port = 3000;
 
-// Instancias para la conexion a la base de datos
 const database = new Database();
 const userRepository = new UserRepository(database);
 
-// Rutas
 app.get('/user/:id', getUserController(userRepository));
 app.get('/users', getUsersController(userRepository));
 app.post('/user/create', createUserController(userRepository));
